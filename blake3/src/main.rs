41186c587c4e72166fa41ce23fd3db9fe703893b
Undiscovered-Data <chaserbrummel@gmail.com>
2022-02-12 23:12:08 -0600

    Update main.rs

--- blake3/src/main.rs
+++ blake3/src/main.rs
@@ -2,5 +2,15 @@
 
 fn main() {
     let hash = blake3::hash(b"hello");
+    println!("The blake3 hash for \"hello\" is:");
     println!("{}", hash);
+    
+    //You can also do this
+    /*
+    let mut the_hash = blake3::Hasher::new();
+    the_hash.update(b"hello");
+    let res = the_hash.finalize();
+    println!("The blake3 hash for \"hello\" is:");
+    ptinln!("{}", hash);
+    */
 }
