1227b4cdf0d18678af7235816cfe2847948efe6b
Undiscovered-Data <chaserbrummel@gmail.com>
2022-02-13 23:43:47 -0600

    Update main.rs

--- md5/src/main.rs
+++ md5/src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     let mut hasher = Md5::new();
     hasher.update("Hello");
     let res = hasher.finalize();
-    println!("{:x}", res);
+    println!("\nThe Md5 hash for \"Hello\" is:");
+    println!("{:x}\n", res);
 }
