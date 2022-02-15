1c3d2e048cdb64445ebb26f39a248f16af7a355d
Undiscovered-Data <chaserbrummel@gmail.com>
2022-02-15 00:29:01 -0600

    Update main.rs

--- whirlpool/src/main.rs
+++ whirlpool/src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     let mut hasher = Whirlpool::new();
     hasher.update("Hello");
     let res = hasher.finalize();
-    println!("{:x}", res);
+    println!("\nThe Whirlpool hash for \"Hello\" is:");
+    println!("{:x}\n", res);
 }
