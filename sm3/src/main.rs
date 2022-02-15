f7c330e6ea8a45a6058074de361853cbfdfacad5
Undiscovered-Data <chaserbrummel@gmail.com>
2022-02-15 00:17:24 -0600

    Update main.rs

--- sm3/src/main.rs
+++ sm3/src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     let mut hash = Sm3::new();
     hash.update("Hello");
     let res = hash.finalize();
+    println!("\nThe Sm3 hash for \"Hello\" is:");
     println!("{:x}", res);
 }
